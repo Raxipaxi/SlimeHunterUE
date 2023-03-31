20be2dfceb67c552dbdc1e5c977ce10e34a69da1
Raxipaxi <razielferundir@gmail.com>
2023-03-31 11:00:39 -0300

    First push again

---
+++ Source/SlimeHunterUE/SlimeHunterUE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SlimeHunterUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlimeHunterUE, "SlimeHunterUE" );
---
+++ Source/SlimeHunterUE/SlimeHunterUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlimeHunterUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLIMEHUNTERUE_API ASlimeHunterUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
