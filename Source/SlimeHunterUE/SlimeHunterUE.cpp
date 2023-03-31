4c4b255574b1d1d4d3963044b11c237d1230d1f8
Raxipaxi <razielferundir@gmail.com>
2023-03-31 10:51:17 -0300

    Puff reset

--- Source/SlimeHunterUE/SlimeHunterUE.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SlimeHunterUE.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlimeHunterUE, "SlimeHunterUE" );
-
-DEFINE_LOG_CATEGORY(LogSlimeHunterUE)
- 
